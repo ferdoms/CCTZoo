d120e9434874740434b407a40bcd6773f8269912
JPedroHaddad <32636733+JPedroHaddad@users.noreply.github.com>
2018-04-10 18:33:50 +0100

    I tried to do the class keeper but i do not know if i got it right.

--- src/keeper/Keeper.java
+++ src/keeper/Keeper.java
@@ -7,8 +7,41 @@ package keeper;
 
 /**
  *
- * @author fernandoms
+ * @author joao-
  */
 public class Keeper {
+    private String name;
+    private String qualification;
     
+    public Keeper(String name, String qualification){
+       this.name = name;
+       this.qualification = qualification;
+    }
+    /**
+    *simple public method get name to get the name attribute and return it when 
+    *requested by other methods
+    */
+    public String getName(){
+        return name;
+    }
+    /**
+    *simple public method get qualification to get the qualification attribute and return it when 
+    *requested by other methods
+    */
+    public String qualification(){
+        return qualification;
+    }
+    /**
+    *setName method which can create or change any keeper`s name 
+    */
+    public void setName(String Name){
+        this.name = name;
+    }
+    /**
+    *setQualification method which can change any keeper`s qualification or 
+    * create/set qualification to new keeper in the zoo
+    */
+    public void setQualification(String qualification){
+        this.qualification = qualification;
+    }
 }
