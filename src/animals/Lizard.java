2936faccce5ff7f56ac53cb79963fb2f6be433c2
JPedroHaddad <32636733+JPedroHaddad@users.noreply.github.com>
2018-04-10 17:48:54 +0100

    some new classes.

---
+++ src/animals/Lizard.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animals;
+
+import interfaces.Reptile;
+
+/**
+ *
+ * @author joao-
+ */
+public class Lizard extends Animal implements Reptile{
+    
+}
---
+++ src/animals/Wolf.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animals;
+
+import interfaces.Mamal;
+
+/**
+ *
+ * @author joao-
+ */
+public class Wolf extends Animal implements Mamal{
+    
+}
