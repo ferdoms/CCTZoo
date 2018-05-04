0f688b6671231723d6e0187cd172a774a3f3e456
fernandoms <fernandoms@fernandoms-HP>
2018-05-04 15:25:34 +0100

    update arquivo de tasks TODO

--- test/LabTester.java
+++ test/LabTester.java
@@ -63,15 +63,15 @@ public class LabTester {
 //        am.update(animal);
 //        am.list();
 
-//        Controller ctrl = new Controller();
-//        ctrl.start();
-//        while(!ctrl.exit()){
-//        ctrl.update();
-//        }
-
-          String date = new Epoch().date("01", "01", "2001");
-          System.out.println(date);
-        
+        Controller ctrl = new Controller();
+        ctrl.start();
+        while(!ctrl.exit()){
+        ctrl.update();
+        }
+//
+//          String date = new Epoch().date("30", "02", "2001");
+//          System.out.println(date);
+//        
 //           View s = new SearchMenu();
 //           s.quit();
 //           System.out.println(s.getExit());
